0577a11ffeff0282f903ffcb579e3d8d49a46e67
Thomas Schank <DrTom@schank.ch>
2014-12-03 11:01:52 +0100

    Sildes Overview and  Part 1

--- slides/config.rb
+++ slides/config.rb
@@ -50,6 +50,8 @@
 
 # activate :syntax, :line_numbers => false
 
+set :relative_links, true
+
 set :markdown_engine, :kramdown
 
 set :css_dir, 'stylesheets'
@@ -71,7 +73,7 @@ configure :build do
   activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
